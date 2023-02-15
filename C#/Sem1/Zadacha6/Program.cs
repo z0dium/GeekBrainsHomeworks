265282efdd272262646e0d3cbbea77605411f20a
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-15 03:41:25 +0300

    zadacha 6

---
+++ C#/Sem1/Zadacha6/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Clear();
+Console.Write("Введите число для проверки на четность: ");
+int firstNumber = int.Parse(Console.ReadLine());
+if (firstNumber%2 == 0) Console.WriteLine("четное");
+else Console.WriteLine("нечетное");
