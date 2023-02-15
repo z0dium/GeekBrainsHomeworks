8baa43e7e0b7eca638117d281c861ed6c9ddf4f2
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-15 03:22:51 +0300

    2 zadacha

--- C#/Sem1/Zadacha2/Program.cs
+++ C#/Sem1/Zadacha2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+if (firstNumber < secondNumber) Console.Write($"Число {secondNumber} больше, чем число {firstNumber}");
+else if (firstNumber == secondNumber) Console.Write($"Число {secondNumber} равно числу {firstNumber}");
+else Console.Write($"Число {firstNumber} больше, чем число {secondNumber}");
