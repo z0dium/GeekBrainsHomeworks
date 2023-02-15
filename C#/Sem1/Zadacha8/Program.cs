e129e46c2cc7515539627d0516a084835ee48982
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-15 03:51:20 +0300

    zadacha 8

---
+++ C#/Sem1/Zadacha8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 1; i < number + 1; i++)
+{
+    if (i % 2 == 0) {
+        Console.Write(i);
+        if ( number - i > 1) Console.Write(", ");
+    }
+}
