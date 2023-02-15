096397f612edf595757f9f7c11e631a213a3997f
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-16 02:48:36 +0300

    dop zadacha 7

---
+++ C#/Sem1/dop zadacha/Program.cs
@@ -0,0 +1,9 @@
+﻿// Доп. 7. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число N: ");
+int number = int.Parse(Console.ReadLine());
+while((99 > number && number < 1000) ^ (-1000 < number && number < -99)){
+    Console.WriteLine("Введите ТРЕХЗНАЧНОЕ число N: ");
+    number = int.Parse(Console.ReadLine());
+}
+Console.WriteLine(number % 10);
