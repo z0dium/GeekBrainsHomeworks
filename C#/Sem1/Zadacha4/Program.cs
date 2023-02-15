c05e83ad6a9437dd173dd4485e35ca58c5ce53ef
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-15 03:33:25 +0300

    zadacha 4

---
+++ C#/Sem1/Zadacha4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int thirdNumber = int.Parse(Console.ReadLine());
+int max = firstNumber;
+if (secondNumber > max) max = secondNumber;
+if (thirdNumber > max) max = thirdNumber;
+Console.WriteLine(max);
