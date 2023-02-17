eaa2f244eb303448e50a37abbeba3c5aca7712a4
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-18 00:43:17 +0300

    zadacha 15

---
+++ C#/Sem2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+string input = Console.ReadLine();
+int number = int.Parse(input);
+int digits = 0;
+string = "";
+int processing = number;
+if (number < 100 && number > -100) out = "третьей цифры нет";
+while (processing <> 0){
+    digits++;
+    processing = processing/10;
+}
+Console.WriteLine(digits);
---
+++ C#/Sem2/zadacha 15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.WriteLine("Введите цифру N: ");
+int digit = int.Parse(Console.ReadLine());
+while (!(0 < digit && digit < 8)){
+    Console.Write("Введите корректную цифру дня недели: ");
+    digit = int.Parse(Console.ReadLine());
+}
+if (digit >= 6) Console.WriteLine("да");
+else Console.WriteLine("нет");
