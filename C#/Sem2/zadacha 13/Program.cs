d59c338ad4a8b34eee3622b77d3846e2c089421b
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-18 00:20:50 +0300

    dopolneno reshenie

--- C#/Sem2/zadacha 13/Program.cs
+++ C#/Sem2/zadacha 13/Program.cs
@@ -4,17 +4,16 @@ Console.Write("Введите число N: ");
 string input = Console.ReadLine();
 int number = int.Parse(input);
 int digits = 0;
-string result = "";
 int processing = number;
-if (number < 100 && number > -100) result = "третьей цифры нет";
+if (-100 < number && number < 100) Console.WriteLine("третьей цифры нет");
 else {
-    while ((processing != 0) && result.Length == 0){
-    digits = digits + 1;
-    processing = processing/10;
-}
-processing = number;
-for (int i = 0; i<digits - 3; i++){
-    processing = processing/10;
-}
+    while (processing != 0) {
+        digits = digits + 1;
+        processing = processing/10;
+    }
+    processing = number > 0 ? number : -number;
+    for (int i = 0; i<digits - 3; i++){
+        processing = processing/10;
+    }
 Console.WriteLine(processing % 10);
 }
