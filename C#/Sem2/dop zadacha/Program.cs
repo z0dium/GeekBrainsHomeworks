8e70a4c1674746c6f61b4ec8b427747926858289
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-18 01:54:39 +0300

    dop zadacha

---
+++ C#/Sem2/dop zadacha/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+class Program
+{    public static void Main()
+    {
+        try
+        {
+            // Open the text file using a stream reader.
+            using (var sr = new StreamReader("INPUT.TXT"))
+            {   
+                string file = sr.ReadToEnd();
+                string[] parameters = file.Split("\r\n");
+                int count = int.Parse(parameters[0]);
+                string row = parameters[1];
+                int heightPeter = int.Parse(parameters[2]);
+                string[] rowSplitted = new string[count];
+                rowSplitted = row.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                int result = 0;
+
+                for (int i = 0; i < count; i++){
+                    result = result + 1;
+                    if (int.Parse(rowSplitted[i]) < heightPeter) {
+                        File.WriteAllText("OUTPUT.TXT", result + Environment.NewLine);
+                        return;
+                    }  
+                }
+                File.WriteAllText("OUTPUT.TXT", (result+1) + Environment.NewLine);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
