16166c7cb67f4d096816b72cacd12472f919919a
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-19 14:49:02 +0300

    zadacha 28

---
+++ C#/Sem3/zadacha 28/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 28
+Console.Clear();
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(getFactorial(number));
+
+long getFactorial(int number){
+    long result = 1;
+    for ( int i = 1; i <= number; i++){
+        result *= i;
+    }
+    return result;
+}
+
