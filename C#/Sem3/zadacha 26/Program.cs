e334c1a5569d2902ff1ff380455b8fb6fd1ccd20
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-19 14:38:29 +0300

    zadacha 26

---
+++ C#/Sem3/zadacha 26/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 26
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+int getLength(int input){
+    int digits = 0;
+    int processing = number >= 0 ? number:-number;
+    while (processing > 0) {
+        digits = digits + 1;
+        processing = processing / 10;
+    }
+    return digits;
+}
+
+Console.WriteLine(getLength(number));
