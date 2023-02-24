c027f6bfa8ad8536518b7328eb27636ce3c899f2
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-24 12:30:59 +0300

    zadachi 30 and 19

---
+++ C#/Sem3/zadacha 30/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 30
+Console.Clear();
+randomArray();
+
+int[] randomArray() {
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++){
+        array[i] = new Random().Next(2);
+        Console.Write(array[i]);
+    }
+    return array;
+}
---
+++ C#/Sem3/zadacha19/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задача 19
+Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+if (number/10000 == number % 10 && ((number/1000)%10 == (number/10)%10)) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+
