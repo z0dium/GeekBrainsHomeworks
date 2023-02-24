2c665bab6ff640c79050a52d21acec4d42d67616
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-24 14:55:49 +0300

    zadacha 23

---
+++ C#/Sem3/zadacha 23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 1; i <= number; i++){
+    Console.Write(i*i*i);
+    if (i != number) Console.Write(" ");
+}
