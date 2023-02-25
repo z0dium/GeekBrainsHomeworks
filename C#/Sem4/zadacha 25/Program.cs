efdf2638257b2f470db12e965b1e5de685a91e45
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-02-25 16:01:21 +0300

    zadacha 25

---
+++ C#/Sem4/zadacha 25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+Console.Write("Введите число, которое нужно будет возвести в степень: ");
+double number = double.Parse(Console.ReadLine());
+Console.Write("Введите степень для предыдущего числа: ");
+int grade = int.Parse(Console.ReadLine());
+
+double exponentiate(double X, int y){
+    double result = 1;
+    for (int i = 0; i < y; i++){
+        result *= X;
+    }
+    return result;
+}
+
+Console.WriteLine (exponentiate(number, grade));
