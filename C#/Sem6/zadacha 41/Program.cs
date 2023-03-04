05ee8d6abd98e484a66cafb66d1348dff3739b2e
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-03-04 15:46:44 +0300

    zadacha 41

---
+++ C#/Sem6/zadacha 41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+string[] input = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine($"Введено {countPositiveNumbers(parseStringOfNumbers(input))} чисел больше ноля");
+
+int[] parseStringOfNumbers (string[] numbers){
+    int[] result = new int[numbers.Length];
+    for (int i=0; i < numbers.Length; i++){
+        result[i] = int.Parse(numbers[i]);
+    }
+    return result;
+}
+
+int countPositiveNumbers(int[] numbers){
+    int result = 0;
+    for (int i=0; i < numbers.Length; i++){
+        if (numbers[i] > 0) result++;
+    }
+    return result;
+}
