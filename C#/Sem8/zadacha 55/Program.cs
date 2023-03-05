c1b9909e22e4ded08392514e9079e15a67486786
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-03-05 14:57:26 +0300

    zadacha 55

---
+++ C#/Sem8/zadacha 55/Program.cs
@@ -0,0 +1,43 @@
+﻿// **Задача 55:**Задайте двумерный массив.
+// Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+Console.Clear();
+int[,] array = getArray(5,6);
+printArray(array);
+Console.WriteLine();
+transposeArray(array);
+printArray(array);
+
+int[,] getArray(int m, int n){
+    int[,] array = new int[m,n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            array[i,j] = rnd.Next(10,100);
+        }
+    }
+    return array;
+}
+
+void printArray(int[,] array){
+    for (int i = 0; i < array.GetLength(0); i++){
+        for (int j = 0; j < array.GetLength(1); j++){
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void transposeArray(int[,] array){
+    if (array.GetLength(0) != array.GetLength(1)) {
+        Console.WriteLine("Действие с данным массивом невозможно");
+        return;
+    }
+    for (int i=0; i < array.GetLength(0); i++){
+        for (int j=i; j < array.GetLength(1); j++){
+            int temp = array[i,j];
+            array[i,j] = array[j,i];
+            array[j,i] = temp;
+        }
+    }
+}
