3307484e0ce7407f5a892a343f7ad4462d23db25
Pavel Mihnenkov <pamihnenkov@gmail.com>
2023-03-17 17:07:18 +0300

    zadacha 64

---
+++ C#/Sem9/zadacha 64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+printRecurs(n);
+
+void printRecurs(int param){
+    if (param == 1) Console.WriteLine(1);
+    else {
+        Console.Write(param + " ");
+        printRecurs(param-1);
+    }
+}
